2e4ff00ebfd6996f7fa5cd92cfa7b269d6e72845
Kaweesha-mr <110246539+Kaweesha-mr@users.noreply.github.com>
2023-05-14 09:30:28 +0530

    Update Q2.cpp

--- OOC Assignment 1/Q2.cpp
+++ OOC Assignment 1/Q2.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-class Event{
 
+//creating the class
+class Event {
+
+
+    //declearing privet attributes
     int eventID;
     string eventType;
     string themeColor;
     string location;
 
+    //define the public variables(interfaces)
 public:
     void setEventDetails();
     void displayEventDetails();
@@ -17,36 +22,52 @@ public:
 
 };
 
-Event::Event(int id, string type, string color, string location){
+
+//declearing the functions
+Event::Event(int id, string type, string color, string Location) {
 
     eventID = id;
     eventType = type;
     themeColor = color;
-    location = location;
+    location = Location;
 
 }
 
-void Event::setEventLocation(){
-    cout << endl << "Input new location of Event "<<eventID<< endl;
+void Event::setEventLocation() {
+    cout << endl << "Input new location of Event " << eventID <<" = ";
     cin >> location;
 }
 
 
-void Event::displayEventDetails(){
+void Event::displayEventDetails() {
 
-    cout << "EventType = " << eventType << endl;
+    cout << endl<< "EventType = " << eventType << endl;
     cout << "ThemeColor = " << themeColor << endl;
     cout << "Location = " << location << endl;
+    cout << endl;
+    cout << endl;
 
 
 
 }
 
 
-int main(){
+int main() {
+
+    //creating dynamic objects and passing constructor values
+    Event* e1 = new Event(1, "party", "red", "nugegoda");
+    Event* e2 = new Event(2,"wedding","purple","maharagama");
+    Event* e3 = new Event(3,"party","pink","malabe");
+
+    //calling functions required to output the details
+    e1->setEventLocation();
+    e2->setEventLocation();
+    e3->setEventLocation();
 
 
+    e1->displayEventDetails();
+    e2->displayEventDetails();
+    e3->displayEventDetails();
 
-
-    return 0;
+        return 0;
 }
